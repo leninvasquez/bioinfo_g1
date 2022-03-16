6cb1089dcbdc906694af141c5f82d13411fafb8a
leninvasquez <94935358+leninvasquez@users.noreply.github.com>
2022-03-15 22:45:56 -0500

    Update netsize_all.sh

--- Tarea 1 _Unix_Git/netsize_all.sh
+++ Tarea 1 _Unix_Git/netsize_all.sh
@@ -1,1 +1,10 @@
-Tarea ejercicio 1.10.3 parte 2 
+# Tarea del Ejercio 1, Parte 2
+# Segunda parte del deber Nº 1  de  Bioinformática
+## Escriba un guión que imprima el número de filas y columnas para cada red
+#### - A continuación los comandos utilizados para resolver la segunda parte del deber
+for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; do
+
+- El uso del bucle for permite replicar una tarea determinada
+- El bucle for esta compuesto por "do" y "done". Que son necesarioos para su ejecución
+- ;: El punto y coma permiten escribir el codigo de manera continua
+- grep: Este comando permite buscar una palabra o patrón, para poder imprimir la línea o líneas que este contenga
