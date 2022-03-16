81307c62d8eb56be44f248c6a7ea330af93cbfcd
leninvasquez <94935358+leninvasquez@users.noreply.github.com>
2022-03-15 22:39:10 -0500

    Update netsize.sh
    parte 1

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,2 +1,12 @@
 
-cut -d ";" -f 2-3 | sort
+# Primera parte del deber Nº 1  de  Bioinformática
+
+## Escriba un guión que tome uno de estos archivos y determine el número de filas (polinizadores) y columnas (plantas).
+
+- A continuación los comandos utilizados para resolver la primera parte del deber
+
+- cat n16.txt
+
+- cat ../Saavedra2013/n16.txt | wc -l
+
+- head -n1 ../Saavedra2013/n16.txt | tr -d " " | trs -d "\ n" | wc -c
