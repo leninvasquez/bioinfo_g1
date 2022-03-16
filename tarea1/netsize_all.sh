d1b52ba0af8e258a6e3061bb1f71030b3b8fd44e
leninvasquez <94935358+leninvasquez@users.noreply.github.com>
2022-03-15 22:40:59 -0500

    Update netsize_all.sh
    parte 2

--- tarea1/netsize_all.sh
+++ tarea1/netsize_all.sh
@@ -1,3 +1,5 @@
-
-
 # Tarea del Ejercio 1.10.3 Parte 2
+# Segunda parte del deber Nº 1  de  Bioinformática
+## Escriba un guión que imprima el número de filas y columnas para cada red
+#### - A continuación los comandos utilizados para resolver la segunda parte del deber
+for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; do
