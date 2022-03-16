93bfc4937db8ec58a93fc2ba02967ad09c00d852
leninvasquez <94935358+leninvasquez@users.noreply.github.com>
2022-03-15 22:46:18 -0500

    Update netsize_all.sh
    parte 2

--- Tarea 1 _Unix_Git/netsize_all.sh
+++ Tarea 1 _Unix_Git/netsize_all.sh
@@ -1,7 +1,7 @@
 # Tarea del Ejercio 1, Parte 2
 # Segunda parte del deber Nº 1  de  Bioinformática
 ## Escriba un guión que imprima el número de filas y columnas para cada red
-#### - A continuación los comandos utilizados para resolver la segunda parte del deber
+### - A continuación los comandos utilizados para resolver la segunda parte del deber
 for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; do
 
 - El uso del bucle for permite replicar una tarea determinada
